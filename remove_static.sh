f77b9f438fa4d9f2559e2a6e1be965e09aa8ca11
bnbong <bbbong9@gmail.com>
2022-06-08 17:27:12 +0900

    add  auto  script

---
+++ remove_static.sh
@@ -0,0 +1,6 @@
+git update-index --assume-unchanged posts/**/**/*.jpg
+git update-index --assume-unchanged posts/**/**/*.png
+git update-index --assume-unchanged posts/**/**/*.gif
+rm posts/**/**/*.jpg
+rm posts/**/**/*.png
+rm posts/**/**/*.gif
